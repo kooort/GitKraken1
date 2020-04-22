2c394c41b7af8018dce3c53495b29bcfb438c031
kooort <kooort@wp.pl>
2020-04-22 18:47:05 +0200

    Dodanie przycisku
    Dodanie przycisku i komunikatu

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,3 +1,9 @@
 window.onload = function() {
     
 }
+
+window.onload = function(){
+    var przyciskInformacja = document.getElementById("informacja");
+    przyciskInformacja.addEventListener("click", function(){
+    alert("GitKraken to przyjazny program!");
+    }
